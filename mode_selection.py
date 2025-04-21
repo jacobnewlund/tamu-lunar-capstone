f2c93478decb9858610e37368dd2535155cda97c
jacobnewlund <jacobnewlund@tamu.edu>
2025-04-21 13:41:44 -0500

    implements Carl Optimization into the mode selection, sorry, the implmentation is kinda gnarly

--- mode_selection.py
+++ mode_selection.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+import csv
 class Segment:
     def __init__(self, type, start_ind, end_ind, length):
         self.type = type
@@ -23,7 +23,7 @@ def havDist(lat1, lat2, long1, long2, radius):
     return 2*radius*np.arcsin(np.sqrt((1 - np.cos(lat2-lat1) + np.cos(lat1)*np.cos(lat2)*(1 - np.cos(long2-long1)))/2))
 
 
-def mode_optimization(pathfile):
+def segmentation(pathfile):
     #pathfile needs to be formatted as [index, lat, long, -, grade, action]
     df = pd.read_csv(pathfile)
     path_points = df.to_numpy()
@@ -106,7 +106,75 @@ def mode_optimization(pathfile):
         data['end'].append(seg.end)
         data['length'].append(seg.length)
 
+    return data
+
+def mode_optimization(data):
+    df = pd.DataFrame(data)
+    df.to_csv('path_segments.csv',index=False)
+    # this is genuinely aborrent but I don't know how to work with pandas and honestly I don't want to learn it
+    newCSV = []
+    with open('path_segments.csv', mode='r') as file:
+        fileReader = csv.reader(file, delimiter=',')
+        next(fileReader, None) # get rid of header
+        fileRows = []
+        for row in fileReader:
+            fileRows.append(row)
+
+    # now re-read and merge together segments if possible using the Carl Optimization.
+    for row in fileRows:
+        if row[0] == "straight" and float(row[3]) < 1000.0: # implement CO
+            row[0] = "curve"
+    # Now we merge together same adjacent segment (and sum distances)
+    lastRow = fileRows[0]
+    newCSV.append(lastRow)
+    runningDistance = 0
+    runningIndex = 0
+    mergeMode = False # false means we're not merging.
+    for row in fileRows[1:]: # run along path and attempt to merge together adjacent paths. this is semi-hardcoded and poorly designed
+        lastType = lastRow[0]
+        lastStartIndex = lastRow[1]
+        lastEndIndex = lastRow[2]
+        lastDist = float(lastRow[3])
+        
+        rowType = row[0]
+        rowStartIndex = row[1]
+        rowEndIndex = row[2]
+        rowDist = float(row[3])
+
+        
+
+        if lastType != rowType and mergeMode: # end of merge mode. append to list
+            runningDistance += lastDist
+            newCSV.append([lastType, runningIndex, rowStartIndex, runningDistance])
+            mergeMode = False
+            runningDistance = 0
+            runningIndex = 0
+        elif lastType == rowType: # then we want to merge. Hold off on appending to row
+            mergeMode = True # we're now merging. Until lastType != rowType
+            runningIndex = newCSV[-1][2]
+            runningDistance += lastDist
+        else: # if we aren't merging, just append
+            newCSV.append(lastRow)
+        lastRow = row
+    
+    newCSV.pop(0) # how'd that get in there (also, rare pop() value)
+
+    # for row in newCSV:
+    #     print(row)
+
+    data = {'type' : [],
+            'start' : [],
+            'end' : [],
+            'length' : []}                
+    for row in newCSV:
+        data['type'].append(row[0])
+        data['start'].append(row[1])
+        data['end'].append(row[2])
+        data['length'].append(row[3])
+
     df = pd.DataFrame(data)
     df.to_csv('path_segments.csv',index=False)
 
-mode_optimization('A_star_Path.csv')
+data = segmentation('A_star_Path.csv')
+mode_optimization(data)
+print("Done creating .csv")
